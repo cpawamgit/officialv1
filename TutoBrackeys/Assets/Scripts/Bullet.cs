4ab34046d57548eb454c6f6f9c08df6e2398d718
Pico-PC\Administrateur <nicolaspierrot@hotmail.fr>
2018-04-26 19:50:42 +0200

    projet

---
+++ TutoBrackeys/Assets/Scripts/Bullet.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform target;
+
+    public float speed = 70f;
+    public int damage = 50;
+
+    public float explosionRadius = 0f;
+
+    public GameObject impactEffect;
+
+    public void Seek (Transform _target)
+    {
+        target = _target;
+    }
+	void Update () {
+		if (target == null)
+        {
+            MyObjectPooler.Instance.ReturnToPool(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
+	}
+
+    void HitTarget()
+    {
+        GameObject impactEffectInst = MyObjectPooler.Instance.SpawnFromPool(impactEffect);
+        impactEffectInst.transform.position = transform.position;
+        impactEffectInst.transform.rotation = transform.rotation;
+        impactEffectInst.SetActive(true);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }else
+        {
+            Damage(target);
+        }
+
+        MyObjectPooler.Instance.ReturnToPool(gameObject);
+
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+
+    }
+
+    void Damage (Transform enemy)
+    {
+        Enemy e = enemy.GetComponent<Enemy>();
+
+        if (e != null)
+        {
+            e.TakeDamage(damage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
---
+++ TutoBrackeys/Assets/Scripts/Enemy.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float startSpeed = 10f;
+    public float startHealth = 100f;
+    private float health;
+
+    [HideInInspector]
+    public float normalizedHealth;
+
+    private EnemyHealthBar enemyHealthBar;
+
+    public int value = 50;
+    public GameObject deathEffect;
+
+    [HideInInspector]
+    public float speed;
+
+    public float CDBetweenSpawns;
+    public int price;
+
+    private bool isDead = false;    
+
+    public Alignement alignement;
+
+    public Sprite image;
+
+    //public Animator animator;
+
+    //private void Awake()
+    //{
+    //    if (GetComponent<Animator>())
+    //    {
+    //        animator = GetComponent<Animator>();
+    //    }
+    //}
+
+
+    private void OnEnable()
+    {
+        speed = startSpeed;
+        health = startHealth;
+        enemyHealthBar = GetComponentInChildren<EnemyHealthBar>();
+        isDead = false;
+    }
+
+    //private void Start()
+    //{
+    //    speed = startSpeed;
+    //    health = startHealth;
+    //    enemyHealthBar = GetComponentInChildren<EnemyHealthBar>();
+
+
+    //    //animator.SetBool("isWalking", true);
+
+    //    //animator.Play("Armature|ArmatureAction.001");
+    //}
+
+
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        normalizedHealth = health / startHealth;
+        enemyHealthBar.UpdateEnnemyHealth(normalizedHealth);
+
+        if (health <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+    public void Slow (float pct)
+    {
+        speed = startSpeed * (1f - pct);
+    }
+
+    void Die()
+    {
+        isDead = true;
+
+        GameObject deathEffectInst = MyObjectPooler.Instance.SpawnFromPool(deathEffect);
+        deathEffectInst.transform.position = transform.position;
+        deathEffectInst.transform.rotation = transform.rotation;
+        deathEffectInst.SetActive(true);
+
+        PlayerStats.Instance.ChangeMoney(value);
+
+        WaveSpawner.EnemiesAlive--;
+
+        MyObjectPooler.Instance.ReturnToPool(gameObject);
+    }
+
+}
