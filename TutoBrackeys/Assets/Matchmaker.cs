fbb603e6ee37f742561305aababcabbde94f5b8d
Pico-PC\Administrateur <nicolaspierrot@hotmail.fr>
2018-05-10 10:52:50 +0200

    update pour Ro, début de network

---
+++ TutoBrackeys/Assets/Matchmaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Matchmaker : NetworkManager
+{
+    public void StartHostButton()
+    {
+       StartHost();  
+    }
+
+
+    public void StartClientButton()
+    {
+        StartClient();   
+    }
+
+}
