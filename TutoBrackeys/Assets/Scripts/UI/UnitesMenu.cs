6774f01b770f9d4f1f24d03b6279d21b2d458e7b
cpawamgit <cyril.morin.tai@gmail.com>
2018-05-22 15:37:12 +0200

    push clean2

---
+++ TutoBrackeys/Assets/Scripts/UI/UnitesMenu.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitesMenu : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public List<Image> agentButtonsImageList;
+    public List<Button> agentButtonsList;
+    public List<Text> agentButtonsTextsList;
+
+    private List<int> priceList = new List<int>(5);
+    private List<float> CDList = new List<float>(5);
+    private List<bool> onCDList = new List<bool>(5);
+
+
+    private void Awake()
+    {
+
+        for (int i = 0; i < AgentSelector.Instance.selectedAgents.Count; i++)
+        {
+            agentButtonsImageList[i].sprite = AgentSelector.Instance.selectedAgents[i].GetComponent<Unite>().image;
+            CDList.Add(AgentSelector.Instance.selectedAgents[i].GetComponent<Unite>().CDBetweenSpawns);
+            priceList.Add(AgentSelector.Instance.selectedAgents[i].GetComponent<Unite>().price);
+            onCDList.Add(false);
+        }
+
+    }
+
+
+    private void Update()
+    {
+        int i = 0;
+
+        foreach (Button button in agentButtonsList)
+        {
+            if (onCDList[i])
+            {
+                CDList[i] -= Time.deltaTime;
+                agentButtonsTextsList[i].text = string.Format("{0:00.00}", CDList[i]);
+
+                if (CDList[i] <= 0)
+                {
+                    onCDList[i] = false;
+                    CDList[i] = AgentSelector.Instance.selectedAgents[i].GetComponent<Unite>().CDBetweenSpawns;
+                    agentButtonsTextsList[i].text = null;
+                    if (PlayerStats.Instance.Money > priceList[i])
+                    {
+                        button.interactable = true;
+                    }                 
+                }
+            }
+            i++;
+        }
+    }
+
+
+    public void SpawnUnite(int indexUniteToSpawn)
+    {
+        MyObjectPooler.Instance.SpawnFromPoolAt(AgentSelector.Instance.selectedAgents[indexUniteToSpawn], spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+        onCDList[indexUniteToSpawn] = true;
+        agentButtonsList[indexUniteToSpawn].interactable = false;
+
+
+        PlayerStats.Instance.ChangeMoney(-priceList[indexUniteToSpawn]);
+    }
+	
+
+    public void CheckMoney()
+    {
+        int i = 0;
+
+        foreach (Button button in agentButtonsList)
+        {
+            if (PlayerStats.Instance.Money < priceList[i])
+            {
+                if (button.interactable == true)
+                    button.interactable = false;
+            }
+            else if (onCDList[i] == false)
+            {
+                if (button.interactable == false)
+                    button.interactable = true;
+            }
+
+            i++;
+        }
+    }
+
+}
---
+++ TutoBrackeys/Assets/Scripts/Unite/Unite.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Events;
+
+
+
+
+public class Unite : MonoBehaviour, IDamageable
+{
+    public UnityAction takeDamageAction;
+    public UnityAction attackAction;
+
+    [Header("Stats")]
+    public float fireRate = 1f;
+    public GameObject bulletPrefab; // must be ignored if melee ,  faire un CustomEditor (https://docs.unity3d.com/ScriptReference/Editor.OnInspectorGUI.html) pour hide/show ref suivant le bool
+    public int value = 50;
+    public float CDBetweenSpawns;
+    public int price;
+    [SerializeField]
+    private float baseResistance = 0f;
+    public float BaseResistance
+    { get { return baseResistance; } }
+    private float resistance;
+    public float Resistance
+    { get { return resistance; } }
+
+    [SerializeField]
+    public bool range = false;
+    public bool melee = false;
+
+    public int AAdamage; //auto attack damage, must be ignored if range , faire un CustomEditor (https://docs.unity3d.com/ScriptReference/Editor.OnInspectorGUI.html) pour hide/show ref suivant le bool
+
+    public float startSpeed = 10f;
+    public float startHealth = 100f;
+    [HideInInspector]
+    public float normalizedHealth;
+
+    [Header("Effects")]
+    public Sprite image;
+    public GameObject speedEffect;
+    public GameObject healEffect;
+    public GameObject deathEffect;
+
+    [Header("Setup")]
+    public Targetter targetter;
+
+
+    [HideInInspector]
+    public float speed;
+    [HideInInspector]
+    public Animator anim;
+
+
+    private bool isDead = false;
+    private float fireCountdown = 0f;
+    private float health;
+
+   
+
+    private EnemyHealthBar enemyHealthBar;
+
+
+    private Dictionary<string, GameObject> effectDictionnary;
+
+
+
+    protected virtual void Awake()
+    {
+
+    }
+   
+
+    private void OnEnable()
+    {
+        anim = GetComponent<Animator>();
+        resistance = baseResistance;
+        speed = startSpeed;
+        health = startHealth;
+        enemyHealthBar = GetComponentInChildren<EnemyHealthBar>();
+        isDead = false;
+        normalizedHealth = health / startHealth;
+        enemyHealthBar.UpdateEnnemyHealth(normalizedHealth);
+
+        effectDictionnary = new Dictionary<string, GameObject>();
+        effectDictionnary.Add("speedEffect", speedEffect);
+        effectDictionnary.Add("healEffect", healEffect);
+    }
+
+
+    protected virtual void Update()
+    {
+        if (targetter.target == null)
+        {
+            return;
+        }
+
+
+        targetter.LockOnTarget();
+
+        if (fireCountdown <= 0f)
+        {
+            Attack();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+
+
+
+    protected  virtual void Attack()
+    {
+        if (attackAction != null)
+        {
+            Debug.Log("fire attackAction");
+            attackAction();
+        }
+
+
+        if (range)
+        {
+            GameObject bulletGO = MyObjectPooler.Instance.SpawnFromPool(bulletPrefab);
+            bulletGO.transform.position = targetter.firePoint.position;
+            bulletGO.transform.rotation = targetter.firePoint.rotation;
+            bulletGO.SetActive(true);
+
+            Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+            if (bullet != null)
+                bullet.Seek(targetter.target);
+        }
+
+        if (melee)
+        {
+            targetter.targetEnemy.TakeDamage(AAdamage);
+        }
+  
+
+    }
+
+
+
+
+    public virtual void TakeDamage(float amount)
+    {
+        if (takeDamageAction != null)
+        {
+            takeDamageAction();
+        }
+
+        Debug.Log("TakeDamage amount = " + amount, this);
+
+        float effectivDamage = amount * (1 - resistance);
+
+        Debug.Log("TakeDamage effectivDamage = " + effectivDamage, this);
+
+
+        health -= effectivDamage;
+        normalizedHealth = health / startHealth;
+        enemyHealthBar.UpdateEnnemyHealth(normalizedHealth);
+
+        if (health <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+    public void ModifySpeed (float multiplier)
+    {
+        speed = startSpeed * multiplier;
+    }
+
+    void Die()
+    {
+        isDead = true;
+
+        GameObject deathEffectInst = MyObjectPooler.Instance.SpawnFromPool(deathEffect);
+        deathEffectInst.transform.position = transform.position;
+        deathEffectInst.transform.rotation = transform.rotation;
+        deathEffectInst.SetActive(true);
+
+        PlayerStats.Instance.ChangeMoney(value);
+
+        WaveSpawner.EnemiesAlive--;
+
+        MyObjectPooler.Instance.ReturnToPool(gameObject);
+    }
+
+    public Alignement GetAlignement()
+    {
+        return targetter.alignement;
+    }
+
+
+    public void Heal(float amount)
+    {
+        if (health + amount <= startHealth)
+            health += amount;
+        else
+            health = startHealth;
+
+        normalizedHealth = health / startHealth;
+        enemyHealthBar.UpdateEnnemyHealth(normalizedHealth);
+    }
+
+    public void TurnOnOffEffects(string effect, bool stateToTurn)
+    {
+        if (effectDictionnary[effect] == null)
+        {
+            Debug.Log("No effect with name " + effect);
+            return;
+        }
+
+        if (stateToTurn)
+            effectDictionnary[effect].SetActive(true);
+        else
+            effectDictionnary[effect].SetActive(false);
+
+    }
+
+   public void ModifyRes(float newRes)
+    {
+        resistance = newRes;
+    }
+
+}
