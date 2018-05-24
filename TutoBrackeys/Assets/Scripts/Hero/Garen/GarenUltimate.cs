e136dfb7e4f013d9dbb8fcf6c685fc2c6d91609d
Pico-PC\Administrateur <nicolaspierrot@hotmail.fr>
2018-05-24 12:32:15 +0200

    garen ulti

---
+++ TutoBrackeys/Assets/Scripts/Hero/Garen/GarenUltimate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarenUltimate : Skills {
+
+    
+
+    public int ultimateDamage = 1000;
+
+    public UltimateTargetter ultimateTargetter;
+
+    //private void UpdateTargets()
+    //{
+       
+    //}
+
+    protected override void Update()
+    {
+        base.Update();
+        
+        if (Input.GetKeyDown(KeyCode.R) && skill == skill.dispo)
+        {
+            Debug.Log("R been pressed !");
+            foreach (GameObject enemy in ultimateTargetter.enemies)
+            {
+                IDamageable toDamage = enemy.GetComponent<IDamageable>();
+                toDamage.TakeDamage(ultimateDamage);
+            }
+            skill = skill.inCD;
+        }
+    }
+
+    
+}
